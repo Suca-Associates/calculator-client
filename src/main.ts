c3df4891d7bbbeb2f2a44b889bc11a57d6a9a652
suca19 <carlos.enrique.suca@gmail.com>
2023-06-13 12:16:31 +0100

    Basic calculator html, css and js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import calculator from './calculator.svg'
-import viteLogo from '/vite.svg'
+
 import { setupCounter } from './counter.ts'
 
 
@@ -17,9 +17,32 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       In Development
     </p>
 
-
+    <div class="calculator">
+        <input type="text" id="result" readonly>
+        <input type="button" value="1" onclick="appendToResult('1')">
+        <input type="button" value="2" onclick="appendToResult('2')">
+        <input type="button" value="3" onclick="appendToResult('3')">
+        <input type="button" value="+" onclick="appendToResult('+')">
+        <br>
+        <input type="button" value="4" onclick="appendToResult('4')">
+        <input type="button" value="5" onclick="appendToResult('5')">
+        <input type="button" value="6" onclick="appendToResult('6')">
+        <input type="button" value="-" onclick="appendToResult('-')">
+        <br>
+        <input type="button" value="7" onclick="appendToResult('7')">
+        <input type="button" value="8" onclick="appendToResult('8')">
+        <input type="button" value="9" onclick="appendToResult('9')">
+        <input type="button" value="*" onclick="appendToResult('*')">
+        <br>
+        <input type="button" value="0" onclick="appendToResult('0')">
+        <input type="button" value="C" onclick="clearResult()">
+        <input type="button" value="=" onclick="calculateResult()">
+        <input type="button" value="/" onclick="appendToResult('/')">
+    </div>
     
   </div>
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+//@ts-nocheck
+function appendToResult(value: string) {
+    document.getElementById('result').value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  function calculateResult() {
+    var result = document.getElementById('result').value;
+    var answer = eval(result);
+    document.getElementById('result').value = answer;
+  }
