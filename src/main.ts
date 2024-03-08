fd4ae9a3243e0ddab90f3f17b7cf96877fef84c4
Carlos Suca <carlos.enrique.suca@gmail.com>
2024-03-08 18:12:58 +0000

    adding a function image

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import {render,} from 'lit-html'
+import { render, } from 'lit-html'
 import './style.css'
 import calculator from './calculator.svg'
 import { setupCounter } from './components/Counter/counter.ts'
-import {bodyTemplate} from './components/CalculatorCells/calculatorCells.tpl.ts';
+import { bodyTemplate } from './components/CalculatorCells/calculatorCells.tpl.ts';
 import { headerTemplate } from './components/Header/header.tpl.ts';
-import {footerTemplate} from './components/Footer/footer.tpl.ts';
+import { footerTemplate } from './components/Footer/footer.tpl.ts';
 import { setupButtons } from './setupButtons.ts';
-import {CalCulatorCells} from './components/CalculatorCells/CalculatorCells.ts';
+import { CalCulatorCells } from './components/CalculatorCells/CalculatorCells.ts';
 //Append Elements to the DOM
 render(headerTemplate(calculator), document.querySelector('#header') as HTMLDivElement);
-render(bodyTemplate, document.querySelector('#body') as HTMLDivElement );
+render(bodyTemplate, document.querySelector('#body') as HTMLDivElement);
 render(footerTemplate, document.querySelector('#footer') as HTMLDivElement);
 
 //Functions
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
-//setupButtons(document.querySelectorAll<HTMLInputElement>('.button--number'));
+setupButtons(document.querySelectorAll<HTMLInputElement>('.button--number'));
 
 const cells = new CalCulatorCells();
 cells.run();
